29a45d658f07e71874327383a7d23dd0c6b22350
Richard Ward <RichieS@GMail.com>
2023-02-06 22:33:03 +0000

    remove useless middleware

--- src/setup/routing.options.ts
+++ src/setup/routing.options.ts
@@ -1,13 +1,10 @@
 import { RoutingControllersOptions } from 'routing-controllers';
 import * as controllers from '../controllers';
-import * as middlewares from './routing.middlewares';
 import { dictToArray } from './utils';
 import { serverSettings } from '../config';
 
 export const routingConfigs: RoutingControllersOptions = {
     controllers: dictToArray(controllers),
-
-    middlewares: dictToArray(middlewares),
 
     routePrefix: serverSettings.context,
 
